a263beaef44ed4b9ff92484624309844c3c2b6fe
HaidenWhite <0whith69@solent.ac.uk>
2025-11-14 10:13:09 +0000

    Tasks 1 to 4

--- composeApp/src/jvmMain/kotlin/org/example/project/App.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/App.kt
@@ -1,49 +1,47 @@
 package org.example.project
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import week8project.composeapp.generated.resources.Res
-import week8project.composeapp.generated.resources.compose_multiplatform
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        var stateList = remember { mutableStateListOf<Student>() }
+    }
+}
+
+@Composable
+fun StudentList(studentList: List<Student>) {
+    Column{
+        studentList.forEach {
+            Text("$it")
         }
     }
+}
+
+@Composable
+fun AddStudent(studentList: List<Student>) {
+    var studentId = remember { mutableStateOf("") }
+    var name = remember { mutableStateOf("") }
+    var course = remember { mutableStateOf("") }
+    var mark = remember { mutableStateOf("") }
+    Column {
+        TextField(value = studentId.value, onValueChange = { studentId.value = it }, label = { Text("Student ID") })
+        TextField(value = name.value, onValueChange = { name.value = it }, label = { Text("Name") })
+        TextField(value = course.value, onValueChange = { course.value = it }, label = { Text("Course") })
+        TextField(value = mark.value, onValueChange = { mark.value = it }, label = { Text("Mark") })
+        Button(onClick = {
+            val currentStudent = Undergraduate(id = studentId.value, name = name.value, course = course.value)
+            currentStudent.mark = mark.value.toInt()
+            studentList.add(currentStudent)
+        }) {
+            Text("Add Student")
+        }
+        StudentList(studentList.toList())
 }
